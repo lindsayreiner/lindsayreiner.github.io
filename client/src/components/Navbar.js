7914edcab3fdf7b2680eef2717ead7e6298f81a7
Lindsay Cristina Reiner <lindsayreiner@hey.com>
2021-12-30 00:47:02 -0500

    started creating navbar, added material UI and started styling

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,27 +1,40 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 import { HashLink } from "react-router-hash-link"
+import { styled } from '@mui/material/styles';
+import Button from '@mui/material/Button';
+import { blueGrey } from '@mui/material/colors';
+
+import '../styles/App.css'
+
+const ColorButton = styled(Button)(({ theme }) => ({
+    color: theme.palette.getContrastText(blueGrey[500]),
+    backgroundColor: blueGrey[500],
+    '&:hover': {
+        backgroundColor: blueGrey[700],
+    },
+}));
 
 
 export default function Navbar() {
     return (
         <div>
+
             <nav>
+                <Link className="initials animation" to="/">LCR</Link>
                 <ul>
+
                     <li>
-                        <Link className="initials" to="/">LR</Link>
+                        <ColorButton className="navBtn" variant="contained"><Link className="navBtn" to="/about">About</Link></ColorButton>
                     </li>
                     <li>
-                        <Link to="/about">About</Link>
+                        <ColorButton variant="contained"><Link className="navBtn" to="/projects">Projects</Link></ColorButton>
                     </li>
                     <li>
-                        <Link to="/projects">Projects</Link>
+                        <ColorButton variant="contained"><HashLink className="navBtn" to="#contact">Contact</HashLink></ColorButton>
                     </li>
                     <li>
-                        <HashLink to="#contact">Contact</HashLink>
-                    </li>
-                    <li>
-                        <Link to="/resume">Resume</Link>
+                        <ColorButton variant="contained"><Link className="navBtn" to="/resume">Resume</Link></ColorButton>
                     </li>
                 </ul>
             </nav>
