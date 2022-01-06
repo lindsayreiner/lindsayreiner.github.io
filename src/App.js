3265d176d528aacbe9276bf7b35354fcef11b2ba
Lindsay Cristina Reiner <lindsayreiner@hey.com>
2022-01-06 18:18:27 -0500

    pushing portfolio

--- src/App.js
+++ src/App.js
@@ -21,15 +21,17 @@ function App() {
         <main>
           <Routes>
             <Route path="/" element={<About />} />
-            <Route path="/about" element={<About />} />
+            {/* <Route path="/about" element={<About />} /> */}
             <Route path="/portfolio" element={<Portfolio />} />
             <Route path="/resume" element={<Resume />} />
             <Route path="/contact" element={<Contact />} />
+
           </Routes>
         </main>
+        <Footer />
       </Router>
 
-      <Footer />
+
     </>
   );
 }
