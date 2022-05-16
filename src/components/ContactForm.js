e1635b9dcb8db525bde46bff4f2bfb1fac85a325
Lindsay Reiner <lindsayreiner@hey.com>
2022-05-16 12:18:12 -0400

    removed isValidEmail to try to rerun Netlify build

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -40,10 +40,10 @@ export default function ContactForm() {
         }
     }
 
-    const isValidEmail = email => {
-        const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        return regex.test(String(email).toLowerCase());
-    };
+    // const isValidEmail = email => {
+    //     const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    //     return regex.test(String(email).toLowerCase());
+    // };
 
     return (
         <>
